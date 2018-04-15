2da86e70dcb2c36c46acf0487da95f1ec1e78600
OsirisRoman <osirisr1994@gmail.com>
2018-04-15 06:18:28 -0500

    Implementación de un botón ingresar que me lleve dentro de la app donde se podrá observar un menú lateral con una función en el botón perfil vendedor. Pd. no se hace uso del módulo, el link desde donde se consume el servicio rest no es el correcto y toca cambiar, aún no existe el método delete.

--- app/src/main/java/com/example/hugo/yachayfood/Autentificacion.java
+++ app/src/main/java/com/example/hugo/yachayfood/Autentificacion.java
@@ -1,7 +1,9 @@
 package com.example.hugo.yachayfood;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Autentificacion extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class Autentificacion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_autentificacion);
+    }
+
+    public void goToHome(View view){
+        Intent intent = new Intent(this, LateralMenu.class);
+        startActivity(intent);
     }
 }
